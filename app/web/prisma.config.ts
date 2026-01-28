c01e3332732c3e280fb261dde44ad386fa4fbc55
izume01 <shrey16211@gmail.com>
2026-01-28 19:42:15 +0530

    chore(db setup) : working

---
+++ app/web/prisma.config.ts
@@ -0,0 +1,13 @@
+// app/web/prisma.config.ts
+import 'dotenv/config'
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: "../../packages/db/prisma/schema.prisma",
+  migrations: {
+    path: "../../packages/db/prisma/migrations",
+  },
+  datasource: {
+    url: env('DATABASE_URL'),
+  },
+});
